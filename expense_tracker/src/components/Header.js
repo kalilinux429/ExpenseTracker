f25fea4a621ed85e2a284797884df63c4d7e2298
Suchit Kapale <77877663+kalilinux429@users.noreply.github.com>
2024-08-27 03:40:53 -0700

    Update Header.js
    alt logo

--- expense_tracker/src/components/Header.js
+++ expense_tracker/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <h1 className='expense'><img src="accounting.png" />Expense Tracker</h1>
+    <h1 className='expense'><img src="accounting.png" alt="logo"/>Expense Tracker</h1>
     <hr/>
     </>
   )
